bc85ceeb059c645733f697bc6adb18f04c733a63
“tobi-j-a ssh-keygenssh-keygengit config --global user.name “tobi-j-a <tobiakinola01@outlook.comgit config --global user.name tobi-j-a>
2023-04-18 16:56:34 +0100

    further updates

--- server/app.js
+++ server/app.js
@@ -11,4 +11,29 @@ app.get('/', (req, res) => {
     res.send(`Welcome to Image Gang! There are ${data.length} questions available`);
 })
 
+app.get("/data/:id", (req, res) => {
+    const idx = req.params.id;
+  
+    res.send(data[idx - 1]);
+  
+    if (!data) {
+      res.status(404).json({ message: `Data with id ${idx - 1} not found` });
+    } else {
+      res.send(data);
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = app;
--- server/data.js
+++ server/data.js
@@ -1,5 +1,33 @@
 const data = [ {
-    id: 1, question: "Who is the author of this painting?", ans1: "Monet", ans2: "Picasso", ans3: "Da Vinci", img: "url"
-}]
+    id: 1,
+    question: "Who is the author of this painting?",
+    answers: {
+        id: 1, ans: "Monet",
+        id: 2, ans: "Picasso",
+        id: 3, ans: "Da Vinci"
+    },
+    correct_ans_id: 2
+},
+
+{
+    id: 2,
+    question: "Is it raining?",
+    answers: {
+        id: 1, ans: "Monet2",
+        id: 2, ans: "Picasso2",
+        id: 3, ans: "Da Vinci2"
+    },
+    correct_ans_id: 4
+}
+]
+
+
+
+
+
+
+
+
+
 
 module.exports = data 
