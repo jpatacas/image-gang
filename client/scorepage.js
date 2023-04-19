5bf5937ff568e0bdc0a800f41b9f372c55d34376
Francesca Adu <58713149+FrancescaAdu@users.noreply.github.com>
2023-04-19 18:02:24 +0100

    added scorepage html and js

---
+++ client/scorepage.js
@@ -0,0 +1,12 @@
+const randomiseButton = document.querySelector("btn-facts");
+randomiseButton.addEventListener("click", displayRandomFacts);
+
+// Display Random Facts
+async function displayRandomFacts() {
+    const response = await fetch("http://localhost:3000/scorepage")
+    const facts = await response.json();
+    console.log(facts);
+
+    const factElement = document.getElementById("#facts");
+    factElement.textContent = facts["facts"];
+}
