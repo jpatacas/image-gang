37fb3454752bbc40e3c670991e749b1e00ae81c7
George Hill <georgeophill@outlook.com>
2023-04-20 12:27:03 +0100

    added displayImage and displayQuestion functions

--- client/index.js
+++ client/index.js
@@ -1,24 +1,43 @@
 
-// Counter Function Attempt
-function sumScore(data) {
-  let totalScore = 0;
-    for (let i = 0; i < data.length; i++) {
-      totalScore += data[i].correct_ans_id;
-    }
-    return totalScore;
-  }
+// // // Counter Function Attempt
+// function sumScore(data) {
+//   let totalScore = 0;
+//     for (let i = 0; i < data.length; i++) {
+//       totalScore += data[i].correct_ans_id;
+//     }
+//     return totalScore;
+//   }
 
-  const totalScore = sumScore(data);
-  console.log(totalScore);
+//   const totalScore = sumScore(data);
+//   console.log(totalScore);
 
+
+// Display question
+
+async function displayQuestion() {
+  const res = await fetch("http://localhost:3000/data")
+  const data = await res.json()
+
+  const theQuestion = document.querySelector('.question');
+
+  theQuestion.textContent = data[1].question
+}
+displayQuestion()
 
   // Display Image
 
   async function displayImage() {
-    const res = await fetch("http;//localhost:3000/data/iamge")
+    const res = await fetch("http://localhost:3000/data")
     const data = await res.json()
 
-    const theImage = document.querySelector('.imageElement');
+    console.log(image[0].question)
+   document.getElementById('#imageElement').src = data[1].image
 
-    theImage.textContent = data['image']
+    theImage.innerHTML = image[0].image
+
+
+    // var img = document.createElement('img');
+    // img.src = data[1].image;
+    // document.img.appendChild(img);
 }
+displayImage()
