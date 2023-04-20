8012bc43e01fef54d436de47ae3789fff7c5225a
George Hill <georgeophill@outlook.com>
2023-04-20 09:09:10 +0100

    untracked changes

--- client/index.js
+++ client/index.js
@@ -1,13 +1,24 @@
-const image = document.querySelector(img);
 
 // Counter Function Attempt
 function sumScore(data) {
-    let totalScore = 0;
+  let totalScore = 0;
     for (let i = 0; i < data.length; i++) {
-        totalScore += data[i].correct_ans_id;
+      totalScore += data[i].correct_ans_id;
     }
     return totalScore;
-}
+  }
 
-const totalScore = sumScore(data);
-console.log(totalScore);
+  const totalScore = sumScore(data);
+  console.log(totalScore);
+
+
+  // Display Image
+
+  async function displayImage() {
+    const res = await fetch("http;//localhost:3000/data/iamge")
+    const data = await res.json()
+
+    const theImage = document.querySelector('.imageElement');
+
+    theImage.textContent = data['image']
+}
